d223ae2a5811453e961626dfda09c2f84ce02f44
sojib <mdsojibhosen444@gmail.com>
2023-12-26 16:00:09 +0600

    Adding Course Entity

--- aspnet-core/src/Kidoo.Learn.EntityFrameworkCore/Migrations/20231226095750_Adding_Course_Entity.Designer.cs
+++ aspnet-core/src/Kidoo.Learn.EntityFrameworkCore/Migrations/20231226095750_Adding_Course_Entity.Designer.cs
@@ -4,6 +4,7 @@ using Kidoo.Learn.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Kidoo.Learn.Migrations
 {
     [DbContext(typeof(LearnDbContext))]
-    partial class LearnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231226095750_Adding_Course_Entity")]
+    partial class Adding_Course_Entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
